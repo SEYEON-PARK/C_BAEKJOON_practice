1389e7c3e2af99ac7b2053b4ee4474cda586ef8b
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2021-03-27 02:00:03 +0900

    C언어 BAEKJOON 1001번 문제 풀기!(2)

--- C_1001_2.c
+++ C_1001_2.c
@@ -5,7 +5,7 @@ int main(void)
     int A, B; // 변수 생성
     scanf("%d %d", &A, &B); // 사용자로부터 두 정수 입력받기
     
-    printf("%d\n", A-B);
+    printf("%d\n", A-B); // 두 정수의 차 출력하기
     
     return 0;
 }
