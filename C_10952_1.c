9dbb51d9f7876fb29ce1224dedf76f2f8bb26745
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2021-04-20 00:45:28 +0900

    C언어 BAEKJOON 10952번 문제 풀기!(1)

--- C_10952_1.c
+++ C_10952_1.c
@@ -2,17 +2,17 @@
 
 int main(void)
 {
-    int A[100];
-    int B[100];
-    int i=0;
+    int A[100]; // 배열 A 선언
+    int B[100]; // 배열 B 선언
+    int i=0; // 변수 i 선언 및 초기화
     
-    while(1)
+    while(1) // 무한 
     {
-        scanf("%d %d", &A[i], &B[i]);
+        scanf("%d %d", &A[i], &B[i]); // 사용자로부터 A[i], B[i] 값 입력받기
         
         if(A[i]==0 && B[i]==0)
-            break;
-        i++;
+            break; // 센티널 값(0, 0)이 입력되면 반복문 종료!
+        i++; // i에 1 더하기
     }
     
     for(i=0;A[i]!=0 && B[i]!=0;i++)
