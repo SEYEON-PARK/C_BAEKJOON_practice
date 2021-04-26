d44467da0ad9d46bc90722db140b0bfcdc5066a0
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2021-04-27 03:14:39 +0900

    C언어 BAEKJOON 2920번 문제 풀기!(1)

--- C_2920_1.c
+++ C_2920_1.c
@@ -11,27 +11,27 @@ int main(void)
     int A[8];
     for(int i=0;i<8;i++)
     {
-        scanf("%d", &A[i]);
+        scanf("%d", &A[i]); // 숫자 8개 입력받기
     }
     for(int i=0, a=1;i<8;i++)
     {
-        if(A[0]==1)
+        if(A[0]==1) // 만약 입력받은 첫 번째 숫자가 1이라면
         {
-            if(A[i]!=a)
+            if(A[i]!=a) // 만약 입력받은 첫 번째 숫자가 1이고 A[i]가 a와 같지 않다면
             {
-                printf("mixed\n");
-                break;
+                printf("mixed\n"); // "mixed"를 출력하고
+                break; // 반복문을 빠져나가기
             }
-            else if(i==7)
+            else if(i==7) // 만약 입력받은 첫 번쨰 숫자가 1이고 A[i]가 a와 같으며 i가 7이라면
             {
-                printf("ascending\n");
+                printf("ascending\n"); // "ascending"을 출력하기
             }
-            a++;
+            a++; // a에 1 더하기
         }
-        else if(A[0]!=8)
+        else if(A[0]!=8) // 만약 입력받은 첫 번째 숫자가 1이 아니고 8도 아니라면
         {
-            printf("mixed\n");
-            break;
+            printf("mixed\n"); // "mixed"를 출력하고
+            break; // 반복문을 빠져나가기
         }
     }
     for(int i=0, a=8;i<8;i++)
