200af32c43e3d90ac39a051d6e02f997ac22f115
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2021-07-03 00:16:35 +0900

    C언어 BAEKJOON 2742번 문제 풀기!(1)

--- C_2742_1.c
+++ C_2742_1.c
@@ -11,9 +11,9 @@
 int main(void)
 {
     int N; // 변수 선언
-    scanf("%d", &N);
+    scanf("%d", &N); // 사용자로부터 정수 입력받기
     
-    for(int i=N;i>0;i--)
+    for(int i=N;i>0;i--) // i가 N부터 0보다 클 때까지 1씩 감소하며 반복
     {
         printf("%d\n", i);
     }
