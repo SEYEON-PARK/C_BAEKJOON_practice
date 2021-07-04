4037327c5478537e0acc9cdf4d6cc46068e25143
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2021-07-05 05:15:24 +0900

    C언어 BAEKJOON 10872번 문제 풀기!(2)

--- C_10872_2.c
+++ C_10872_2.c
@@ -10,12 +10,12 @@
 
 int main(void)
 {
-    int N, result=1;
-    scanf("%d", &N);
+    int N, result=1; // 변수 선언 및 초기화
+    scanf("%d", &N); // 사용자로부터 정수 입력받기
     
-    for(int i=N;i>=1;i--)
+    for(int i=N;i>=1;i--) // i가 N부터 1보다 크거나 같을 때까지 1씩 감소하며 반복
     {
-        result*=i;
+        result*=i; // result에 result*i 값 대입하기
     }
     
     printf("%d\n", result);
