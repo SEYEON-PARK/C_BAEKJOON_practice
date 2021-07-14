6481543ad892c90351068fece003b0c2ed0a70a6
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2021-07-14 17:31:39 +0900

    C언어 BAEKJOON 10172번 문제 풀기!(1)

---
+++ C_10172_1.c
@@ -0,0 +1,20 @@
+/*
+아래 예제와 같이 개를 출력하시오.
+
+없음.
+
+개를 출력한다.
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("|\\_/|\n");
+    printf("|q p|   /}\n");
+    printf("( 0 )\"\"\"\\\n");
+    printf("|\"^\"`    |\n");
+    printf("||_/=\\\\__|\n");
+
+    return 0;
+}
