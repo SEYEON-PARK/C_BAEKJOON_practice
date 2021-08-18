34a0c8e913e8fd0afbda2e123266ffd07504048e
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2021-08-19 00:53:39 +0900

    C언어 BAEKJOON 2440번 문제 풀기!(1)

--- C_2440_1.c
+++ C_2440_1.c
@@ -10,12 +10,12 @@
 
 int main(void)
 {
-    int N;
-    scanf("%d", &N);
+    int N; // 변수 선언
+    scanf("%d", &N); // 사용자로부터 정수 입력받기
     
-    for(int i=N;i>0;i--)
+    for(int i=N;i>0;i--) // i가 N부터 0보다 클 때까지 1씩 줄어들며 반복
     {
-        for(int j=N;j>0;j--)
+        for(int j=N;j>0;j--) // j가 N부터 0보다 클 때까지 1씩 줄어들며 
         {
             printf("*");
         }
