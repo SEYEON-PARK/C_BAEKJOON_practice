d08b7dada831275e2b5057d3c6cf4f34645d1c78
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2021-10-17 00:37:51 +0900

    C언어 BAEKJOON 3003번 문제 풀기!(1)

--- C_3003_1/C_3003_1.c
+++ C_3003_1/C_3003_1.c
@@ -6,4 +6,30 @@
 
 첫째 줄에 동혁이가 찾은 흰색 킹, 퀸, 룩, 비숍, 나이트, 폰의 개수가 주어진다. 이 값은 0보다 크거나 같고 10보다 작거나 같은 정수이다.
 
+첫째 줄에 입력에서 주어진 순서대로 몇 개의 피스를 더하거나 빼야 되는지를 출력한다. 
+만약 수가 양수라면 동혁이는 그 개수 만큼 피스를 더해야 하는 것이고, 음수라면 제거해야 하는 것이다.
 */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int white[6], original[6]={1, 1, 2, 2, 2, 8}, after[6];
+    
+    for(int i=0;i<6;i++)
+    {
+        scanf("%d", &white[i]);
+    }
+    
+    for(int i=0;i<6;i++)
+    {
+        after[i]=original[i]-white[i];
+    }
+    
+    for(int i=0;i<6;i++)
+    {
+        printf("%d ", after[i]);
+    }
+    
+    return 0;
+}
