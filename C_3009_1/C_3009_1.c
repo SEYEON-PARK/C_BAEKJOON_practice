bd569df4d67b0a10072ee30cc787b03a308b7826
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2021-10-23 22:29:05 +0900

    C언어 BAEKJOON 3009번 문제 풀기!(1)

--- C_3009_1/C_3009_1.c
+++ C_3009_1/C_3009_1.c
@@ -6,3 +6,42 @@
 직사각형의 네 번째 점의 좌표를 출력한다.
 */
 
+#include <stdio.h>
+
+int main(void)
+{
+    int x1, y1, x2, y2, x3, y3, x4, y4;
+    scanf("%d %d", &x1, &y1);
+    scanf("%d %d", &x2, &y2);
+    scanf("%d %d", &x3, &y3);
+    
+    if(x1 != x2 && x1 != x3)
+    {
+        x4=x1;
+    }
+    else if(x2 != x1 && x2 != x3)
+    {
+        x4=x2;
+    }
+    else if(x3 != x1 && x3 != x2)
+    {
+        x4=x3;
+    }
+    
+    if(y1 != y2 && y1 != y3)
+    {
+        y4=y1;
+    }
+    else if(y2 != y1 && y2 != y3)
+    {
+        y4=y2;
+    }
+    else if(y3 != y1 && y3 != y1)
+    {
+        y4=y3;
+    }
+    
+    printf("%d %d\n", x4, y4);
+    
+    return 0;
+}
