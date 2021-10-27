b41f6b53c0735f9b2ba35ca8b23bccf7763f3693
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2021-10-27 19:05:38 +0900

    C언어 BAEKJOON 2588번 문제 풀기!(1)

--- C_2588_1/C_2588_1.c
+++ C_2588_1/C_2588_1.c
@@ -14,3 +14,27 @@
 
 첫째 줄부터 넷째 줄까지 차례대로 (3), (4), (5), (6)에 들어갈 값을 출력한다.
 */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int a, b, result[4];
+    scanf("%d", &a);
+    scanf("%d", &b);
+    
+    result[3]=a*b;
+    
+    for(int i=0;i<3;i++)
+    {
+        result[i]=a*(b%10);
+        b=b/10;
+    }
+    
+    for(int i=0;i<4;i++)
+    {
+        printf("%d\n", result[i]);
+    }
+    
+    return 0;
+}
