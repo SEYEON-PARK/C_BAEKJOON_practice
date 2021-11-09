b492eebdfba24522b31c6f99c23f4d8462d5c5dc
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2021-11-10 06:54:59 +0900

    C언어 BAEKJOON 5554번 문제 풀기!(2)

--- C_5554_2/C_5554_2.c
+++ C_5554_2/C_5554_2.c
@@ -13,3 +13,27 @@
 
 총 이동시간 x 분 y 초를 출력한다. 첫 번째 줄에 x를, 두 번째 줄에 y를 출력한다.
 */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int d[4], sum=0, x, y; // 변수 선언
+    for(int i=0;i<4;i++) // i가 0부터 4보다 작을 때까지 1씩 증가하며 반복
+    {
+       scanf("%d", &d[i]); // 사용자로부터 정수 입력받기
+    }
+    
+    for(int i=0;i<4;i++) // i가 0부터 4보다 작을 때까지 1씩 증가하며 반복
+    {
+       sum+=d[i]; // sum에 sum+d[i] 대입하기
+    } 
+    
+    x=sum/60; // x에 sum을 60으로 나눈 몫 대입하기
+    y=sum%60; // y에 sum을 60으로 나눈 나머지 대입하기
+    
+    printf("%d\n", x); // x값 출력하기
+    printf("%d\n", y); // y값 출력하기
+    
+    return 0;
+}
