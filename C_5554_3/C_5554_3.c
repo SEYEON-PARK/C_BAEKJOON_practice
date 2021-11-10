f9cc9d31d457f5c0a0a60bff464a48c6a09a9162
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2021-11-11 06:27:44 +0900

    C언어 BAEKJOON 5554번 문제 풀기!(3)

--- C_5554_3/C_5554_3.c
+++ C_5554_3/C_5554_3.c
@@ -2,18 +2,18 @@
 
 int main(void)
 {
-    int d[4], sum=0, x, y; // 변수 선언
+    int d[4], sum=0, x, y;
     for(int i=0;i<4;i++)
     {
        scanf("%d", &d[i]);
        sum+=d[i]; 
     } 
     
-    x=sum/60; // x에 sum을 60으로 나눈 몫 대입하기
-    y=sum%60; // y에 sum을 60으로 나눈 나머지 대입하기
+    x=sum/60; 
+    y=sum%60;
     
-    printf("%d\n", x); // x값 출력하기
-    printf("%d\n", y); // y값 출력하기
+    printf("%d\n", x); 
+    printf("%d\n", y); 
     
     return 0;
 }
