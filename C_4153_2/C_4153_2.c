fd377073941510196597b6d3070515c88ec46f78
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2021-12-08 00:19:02 +0900

    C언어 BAEKJOON 4153번 문제 풀기!(2)

--- C_4153_2/C_4153_2.c
+++ C_4153_2/C_4153_2.c
@@ -1,5 +1,5 @@
 /*
-과거 이집트인들은 각 변들의 길이가 3, 4, 5인 삼각형이 직각 삼각형인것을 알아냈다. 주어진 세변의 길이로 삼각형이 직각인지 아닌지 구분하시오.
+과거 이집트인들은 각 변들의 길이가 3, 4, 5인 삼각형이 직각 삼각형인것을 알아냈다. 주어진 세 변의 길이로 삼각형이 직각인지 아닌지 구분하시오.
 
 입력은 여러개의 테스트케이스로 주어지며 마지막줄에는 0 0 0이 입력된다. 
 각 테스트케이스는 모두 30,000보다 작은 양의 정수로 주어지며, 각 입력은 변의 길이를 의미한다.
@@ -18,10 +18,10 @@ int main(void)
         scanf("%d %d %d", &a, &b, &c); // 사용자로부터 세 개의 정수 입력받기
         if(a==0 && b==0 && c==0) // a가 0이 아니고 b도 0이 아니고 c도 0이 아니라면
             break; // while문 빠져나가기
-        if(pow((double)a, 2.0)+pow((double)b, 2.0)==pow((double)c, 2.0) || pow((double)a, 2.0)+pow((double)c, 2.0)==pow((double)b, 2.0) || pow((double)b, 2.0)+pow((double)c, 2.0)==pow((double)a, 2.0))
-            printf("right\n");
-        else
-            printf("wrong\n");
+        if(pow((double)a, 2.0)+pow((double)b, 2.0)==pow((double)c, 2.0) || pow((double)a, 2.0)+pow((double)c, 2.0)==pow((double)b, 2.0) || pow((double)b, 2.0)+pow((double)c, 2.0)==pow((double)a, 2.0)) // 주어진 세 변의 길이가 직각 삼각형이라면
+            printf("right\n"); // "right" 출력하기
+        else // 주어진 세 변의 길이가 직각 삼각형이 아니라면
+            printf("wrong\n"); "wrong" 출력하기
     }
     
     return 0;
