45fb92002638af195beb9959cf1eb094c8e68690
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2021-12-09 06:33:08 +0900

    C언어 BAEKJOON 11966번 문제 풀기!(1)

--- C_11966_1/C_11966_1.c
+++ C_11966_1/C_11966_1.c
@@ -5,3 +5,27 @@
 
 N이 2의 제곱수면 1을 아니면 0을 출력하는 프로그램을 작성하시오.
 */
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+    int N;
+    double i=0;
+    scanf("%d", &N);
+    
+    while(1)
+    {
+        if(N<pow(2.0, i))
+            break;
+        if(N==pow(2.0, i))
+        {
+            printf("1\n");
+            return 0;
+        }
+        i++;
+    }
+    printf("0\n");
+    return 0;
+}
