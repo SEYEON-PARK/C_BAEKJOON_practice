b784f902e89d803e5cf7736033a045449aabf266
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2021-12-28 06:23:03 +0900

    C언어 BAEKJOON 5622번 문제 풀기!(2)

--- C_5622_2/C_5622_2.c
+++ C_5622_2/C_5622_2.c
@@ -16,9 +16,9 @@
 
 int main(void)
 {
-    char s[16];
-    scanf("%s", s, sizeof(s));
-    int i=0, number=0;
+    char s[16]; // 문자열 선언
+    scanf("%s", s, sizeof(s)); // 사용자로부터 문자열 입력받기
+    int i=0, number=0; // 정수 선언 및 초기화
     
     while(s[i]!=NULL)
     {
