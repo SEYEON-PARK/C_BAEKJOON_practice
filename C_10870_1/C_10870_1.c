a5f0bb6f4005afd53285cb5a5d226ac1056a232a
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2022-01-16 14:27:24 +0900

    C언어 BAEKJOON 10870번 문제 풀기!(1)

--- C_10870_1/C_10870_1.c
+++ C_10870_1/C_10870_1.c
@@ -9,3 +9,36 @@ n이 주어졌을 때, n번째 피보나치 수를 구하는 프로그램을 작
 
 첫째 줄에 n번째 피보나치 수를 출력한다.
 */
+
+#include <stdio.h>
+
+void f(int i, int *b)
+{
+    b[i]=b[i-2]+b[i-1];
+}
+
+int main(void)
+{
+    int N, i=2;
+    scanf("%d", &N);
+    int B[21]={0, 1};
+    
+    if(N==0)
+    {
+        printf("0");
+        return 0;
+    }
+    else if(N==1)
+    {
+        printf("1");
+        return 0;
+    }
+    
+    for(int j=1;j<N;j++)
+    {
+        f(i, B);
+        i++;
+    }
+    printf("%d", B[N]);
+    return 0;
+}
