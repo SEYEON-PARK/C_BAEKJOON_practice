b995e1c9b4a19ae0cd3ececd4332da81914cc753
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2022-01-17 01:10:31 +0900

    C언어 BAEKJOON 10870번 문제 풀기!(1)

--- C_10870_1/C_10870_1.c
+++ C_10870_1/C_10870_1.c
@@ -19,14 +19,14 @@ void f(int i, int *b) // 함수 f() 선언
 
 int main(void)
 {
-    int N, i=2;
-    scanf("%d", &N);
-    int B[21]={0, 1};
+    int N, i=2; // 변수 선언 및 초기화
+    scanf("%d", &N); // 사용자로부터 정수 입력받기
+    int B[21]={0, 1}; // 배열 선언 및 초기화
     
-    if(N==0)
+    if(N==0) // 만약, N이 0이라면
     {
-        printf("0");
-        return 0;
+        printf("0"); // "0" 출력하기
+        return 0; // 0을 반환하고 프로그램 종료하기
     }
     else if(N==1)
     {
