32c6376bc234400a01b9e12cadef03bd84a1b6dd
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2022-01-18 06:29:14 +0900

    C언어 BAEKJOON 10870번 문제 풀기!(2)

--- C_10870_2/C_10870_2.c
+++ C_10870_2/C_10870_2.c
@@ -1,2 +1,34 @@
 /*
 */
+
+#include <stdio.h>
+
+void f(int i, int *b)
+{
+    b[i]=b[i-2]+b[i-1];
+}
+
+int main(void)
+{
+    int N, i=2;
+    scanf("%d", &N);
+    int B[21]={0, 1};
+    
+    if(N==0)
+    {
+        printf("0");
+        return 0;
+    }
+    else if(N==1)
+    {
+        printf("1");
+        return 0;
+    }
+    
+    for(int j=1;j<N;j++)
+    {
+        f(i++, B);
+    }
+    printf("%d", B[N]);
+    return 0;
+}
