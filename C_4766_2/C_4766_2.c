6903cbf67071f8f9865fead35f7f98c443646260
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2022-01-24 01:39:26 +0900

    C언어 BAEKJOON 4766번 문제 풀기!(2)

--- C_4766_2/C_4766_2.c
+++ C_4766_2/C_4766_2.c
@@ -1,2 +1,30 @@
 /*
+
 */
+#include <stdio.h>
+
+int main(void)
+{
+    int i=0;
+    double f[10000];
+    
+    while(1)
+    {
+        scanf("%lf", &f[i]);
+        if(f[i]==999)
+        {
+            break;
+        }
+        i++;
+    }
+    
+    i=1;
+    
+    for(int j=1;f[j]!=999;j++)
+    {
+        printf("%.2lf\n", f[i]-f[i-1]);
+        ++i;
+    }
+    
+    return 0;
+}
