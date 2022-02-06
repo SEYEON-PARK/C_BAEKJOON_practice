c9802cb80b7fb9689d28299350159f7f8b14eeb3
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2022-02-07 00:57:00 +0900

    C언어 BAEKJOON 10926번 문제 풀기!(2)

--- C_10926_2/C_10926_2.c
+++ C_10926_2/C_10926_2.c
@@ -6,3 +6,16 @@
 
 첫째 줄에 준하의 놀람을 출력한다. 놀람은 아이디 뒤에 ??!를 붙여서 나타낸다.
 */
+
+#include <stdio.h>
+
+int main(void)
+{
+    char id[51];
+    scanf("%s", id, sizeof(id));
+    
+    printf("%s", id);
+    printf("??!");
+    
+    return 0;
+}
