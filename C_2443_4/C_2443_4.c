9a093c6b20a6f67768c69034d3aa1ce88bdba48e
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2022-02-15 21:12:25 +0900

    C언어 BAEKJOON 2443번 문제 풀기!(4)

--- C_2443_4/C_2443_4.c
+++ C_2443_4/C_2443_4.c
@@ -6,3 +6,41 @@
 
 첫째 줄부터 N번째 줄까지 차례대로 별을 출력한다.
 */
+
+#include <stdio.h>
+
+void fun(int num, int t)
+{
+    if(t==0)
+    {
+        for(int x=num;x>0;x--)
+        {
+            printf(" ");
+        }
+    }
+    else
+    {
+        for(int x=num;x>0;x--)
+        {
+            printf("*");
+        }
+    }
+}
+
+
+int main(void)
+{
+    int N, count=1;
+    scanf("%d", &N);
+    
+    for(int i=0;i<N;i++)
+    {
+        fun(i, 0);
+        fun(2*N-count, 1);
+        count+=2;
+        
+        printf("\n");        
+    }
+    
+    return 0;
+}
