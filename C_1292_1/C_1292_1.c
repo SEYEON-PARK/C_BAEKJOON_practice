fdf73bafb329a37da3b207a676faa9b860796600
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2022-03-29 00:34:10 +0900

    C언어 BAEKJOON 1292번 문제 풀기!(1)

--- C_1292_1/C_1292_1.c
+++ C_1292_1/C_1292_1.c
@@ -7,3 +7,36 @@
 
 첫 줄에 구간에 속하는 숫자의 합을 출력한다.
 */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int a=1, b=3, plus=3;
+    int start, end, result=0;
+    scanf("%d %d", &start, &end);
+    
+    if(start==1)
+    {
+        result=1;
+        start++;
+    }
+    
+    while(end>=start)
+    {
+        if(a<start && start<=b)
+        {
+            result+=plus-1;
+            start++;
+        }
+        else
+        {
+            a=b;
+            b+=plus;
+            plus++;
+        }
+    }
+    
+    printf("%d", result);
+    
+}
