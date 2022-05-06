c2caa6593e1dbb27447b05e9f5fba9b9db425e24
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2022-05-06 15:57:24 +0900

    C언어 BAEKJOON 2965번 문제 풀기!(1)

--- C_2965_1/C_2965_1.c
+++ C_2965_1/C_2965_1.c
@@ -7,3 +7,22 @@
 
 캥거루가 최대 몇 번 움직일 수 있는지 출력한다.
 */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int A, B, C;
+    scanf("%d %d %d", &A, &B, &C);
+    
+    if(B-A>C-B)
+    {
+        printf("%d", B-A-1);
+    }
+    else
+    {
+        printf("%d", C-B-1);
+    }
+    
+    return 0;
+}
