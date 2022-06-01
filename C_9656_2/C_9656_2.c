5a3c2fa48e48bb4f9956e359afb99e4cb74b517d
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2022-06-02 01:56:29 +0900

    C언어 BAEKJOON 9656번 문제 풀기!(2)

--- C_9656_2/C_9656_2.c
+++ C_9656_2/C_9656_2.c
@@ -8,3 +8,17 @@
 상근이가 게임을 이기면 SK를, 창영이가 게임을 이기면 CY을 출력한다.
 */
 
+#include <stdio.h>
+
+int main(void)
+{
+    int N;
+    scanf("%d", &N);
+    
+    if(N%2==0)
+        printf("SK");
+    else
+        printf("CY");
+    
+    return 0;
+}
