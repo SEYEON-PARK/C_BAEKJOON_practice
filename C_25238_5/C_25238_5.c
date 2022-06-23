59ce88654a0862f1af5ffd05adfe466fa17558ca
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2022-06-24 02:31:21 +0900

    C언어 BAEKJOON 25238번 문제 풀기!(5)

--- C_25238_5/C_25238_5.c
+++ C_25238_5/C_25238_5.c
@@ -11,3 +11,20 @@
 
 몬스터에게 대미지를 줄 수 있으면 1, 그렇지 않으면 0을 출력해 주세요.
 */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    
+    double result=a-(a*(b/100.0));
+    
+    if(result>=100)
+        printf("0");
+    else
+        printf("1");
+    
+    return 0;
+}
