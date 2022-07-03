918331b381d882dcb0282e21dd570d807ae3a68b
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2022-07-04 01:05:18 +0900

    C언어 BAEKJOON 10996번 문제 풀기!(1)

--- C_10996_1/C_10996_1.c
+++ C_10996_1/C_10996_1.c
@@ -6,3 +6,41 @@
 첫째 줄부터 차례대로 별을 출력한다.
 */
 
+#include <stdio.h>
+
+int main(void)
+{
+    int N, a;
+    scanf("%d", &N);
+    
+    if(N==1)
+    {
+        printf("*");
+        return 0;
+    }
+    
+    for(int i=0;i<2*N;i++)
+    {
+        if(i%2==0)
+            a=1;
+        else
+            a=0;
+            
+        for(int j=0;j<N;j++)
+        {
+            if(a%2==1)
+            {
+                printf("*");
+                a++;
+            }
+            else
+            {
+                printf(" ");
+                a++;
+            }
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
