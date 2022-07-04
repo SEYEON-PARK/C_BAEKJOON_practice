68997e7d236cb81d1eb5b5a30269ca642c115880
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2022-07-05 01:34:31 +0900

    C언어 BAEKJOON 10996번 문제 풀기!(1)

--- C_10996_1/C_10996_1.c
+++ C_10996_1/C_10996_1.c
@@ -10,13 +10,13 @@
 
 int main(void)
 {
-    int N, a;
-    scanf("%d", &N);
+    int N, a; // 변수 선언
+    scanf("%d", &N); // 사용자로부터 정수 입력받기
     
-    if(N==1)
+    if(N==1) // 만약, N이 1이라면
     {
-        printf("*");
-        return 0;
+        printf("*"); // "*" 출력하기
+        return 0; // 프로그램 종료
     }
     
     for(int i=0;i<2*N;i++)
