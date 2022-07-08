8a95b8b3c16d66456a9f164b3036bf90fea73772
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2022-07-09 01:09:02 +0900

    C언어 BAEKJOON 10996번 문제 풀기!(3)

--- C_10996_3/C_10996_3.c
+++ C_10996_3/C_10996_3.c
@@ -15,9 +15,9 @@ int main(void)
 
     for(int i=0;i<2*N;i++) // i가 0부터 2*N이 되기 전까지 1씩 증가하며 반복
     {
-        if(i%2==0)
-            a=1;
-        else
+        if(i%2==0) // i%2의 값이 0과 같다면
+            a=1; // a에 1 대입하기
+        else 
             a=0;
             
         for(int j=0;j<N;j++)
