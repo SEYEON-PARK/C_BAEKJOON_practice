8714ef5bad5d9f38df1b71bce300d4e27a004b6d
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2022-07-21 04:02:21 +0900

    C언어 BAEKJOON 2444번 문제 풀기!(3)

--- C_2444_3/C_2444_3.c
+++ C_2444_3/C_2444_3.c
@@ -10,23 +10,23 @@
 
 int main(void)
 {
-    int N, minus=3;
-    scanf("%d", &N);
+    int N, minus=3; // 변수 선언 및 초기화
+    scanf("%d", &N); // 사용자로부터 정수 입력받기
     
-    for(int i=0;i<2*N-1;i++)
+    for(int i=0;i<2*N-1;i++) // i가 0부터 2*N-1이 되기 전까지 1씩 증가하며 반복
     {
-        if(i<=N-1)
+        if(i<=N-1) // 만약, i가 N-1보다 작거나 같다면
         {
-            for(int j=0;j<N-i-1;j++)
+            for(int j=0;j<N-i-1;j++) // j가 0부터 N-i-1이 되기 전까지 1씩 증가하며 반복
             {
-                printf(" ");
+                printf(" "); // " " 출력하기
             }
-            for(int k=0;k<2*(i+1)-1;k++)
+            for(int k=0;k<2*(i+1)-1;k++) // k가 0부터 2*(i+1)-1이 되기 전까지 1씩 증가하며 반복
             {
-                printf("*");
+                printf("*"); // "*" 출력하기
             }
             
-            printf("\n");
+            printf("\n"); // 한 줄 띄기
         }
         else
         {
