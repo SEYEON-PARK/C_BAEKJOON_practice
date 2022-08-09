3735147b7be4d99a37ee33a29afe1dab58504639
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2022-08-09 23:05:02 +0900

    C언어 BAEKJOON 25083번 문제 풀기!(1)

--- C_25083_1/C_25083_1.c
+++ C_25083_1/C_25083_1.c
@@ -5,3 +5,17 @@
 
 새싹을 출력한다.
 */
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("         ,r'\"7\n");
+    printf("r`-_   ,'  ,/\n");
+    printf(" \\. \". L_r'\n");
+    printf("   `~\\/\n");
+    printf("      |\n");
+    printf("      |\n");
+    
+    return 0;
+}
