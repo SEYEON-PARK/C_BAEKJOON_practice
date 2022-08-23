b3aa6b4873b505d37beeaab4f20020112fb18d03
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2022-08-24 01:24:22 +0900

    C언어 BAEKJOON 25304번 문제 풀기!(1)

--- C_25304_1/C_25304_1.c
+++ C_25304_1/C_25304_1.c
@@ -12,3 +12,29 @@
 
 구매한 물건의 가격과 개수로 계산한 총 금액이 영수증에 적힌 총 금액과 일치하면 Yes를 출력한다. 일치하지 않는다면 No를 출력한다.
 */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int X, N, a, b, sum=0;
+    scanf("%d", &X);
+    scanf("%d", &N);
+
+    for(int i=0;i<N;i++)
+    {
+        scanf("%d %d", &a, &b);
+        sum+=a*b;
+    }
+
+    if(sum==X)
+    {
+        printf("Yes");
+    }
+    else
+    {
+        printf("No");
+    }
+    
+    return 0;
+}
