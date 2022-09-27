10440b7e1e8f976398e6d28ec5d3fc28cb547506
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2022-09-28 00:57:37 +0900

    C언어 BAEKJOON 25625번 문제 풀기!(3)

--- C_25625_3/C_25625_3.c
+++ C_25625_3/C_25625_3.c
@@ -12,3 +12,23 @@
 
 첫째 줄에 민준이가 버스에 탑승하기 위해서 기다려야 하는 최소 시간을 출력한다.
 */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int x, y;
+    scanf("%d %d", &x, &y);
+    
+    if(y>x)
+    {
+        y=y%x;
+        printf("%d", y);
+    }
+    else
+    {
+        printf("%d", x+y);
+    }
+    
+    return 0;
+}
