2a291b52d1f2f9b86721a82f633e14b873c15c28
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2023-02-22 15:14:17 +0900

    C언어 BAEKJOON 5086번 문제 풀기!(1)

--- C_5086_1/C_5086_1.c
+++ C_5086_1/C_5086_1.c
@@ -29,7 +29,7 @@ int main(void)
         
         if(B%A==0) // 만약, B%A의 값이 0과 같다면
         {
-            printf("factor\n");
+            printf("factor\n"); //
         }
         else if(A%B==0)
         {
