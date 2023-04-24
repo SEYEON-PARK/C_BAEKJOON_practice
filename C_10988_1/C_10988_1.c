c656b7d55ba53dd7abcc93144165f4f260bb7750
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2023-04-24 22:36:32 +0900

    C언어 BAEKJOON 10988번 문제 풀기!(1)

--- C_10988_1/C_10988_1.c
+++ C_10988_1/C_10988_1.c
@@ -24,14 +24,14 @@ int main(void)
     for(int i=0;i<=count/2;i++) // i가 0부터 count/2의 값과 작거나 같을 때까지 i를 1씩 증가시키며 반복
     {
         if(s[i]==s[count-i-1]) // 만약, s[i]의 값이 s[count-i-1]의 값과 같다면
-            continue;
-        else
+            continue; // 계속 하기
+        else // s[i]의 값이 s[count-i-1]의 값과 같지 않다면
         {
-            printf("0");
+            printf("0"); // "0" 출력하기
             return 0;
         }
     }
-    printf("1");
+    printf("1"); // "1" 출력하기
     return 0;
     
 }
