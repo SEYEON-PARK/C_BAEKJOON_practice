9c9b2e15124aa9da459a6b716dd6b84f9fd8fdd7
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2023-05-07 23:57:41 +0900

    C언어 BAEKJOON 27323번 문제 풀기!(1)

--- C_27323_1/C_27323_1.c
+++ C_27323_1/C_27323_1.c
@@ -6,3 +6,13 @@
 세로 길이가 A cm, 가로 길이가 B cm인 직사각형의 넓이를 cm2(cm 제곱) 단위로 구하고, 단위 (cm2(cm 제곱))를 생략하여 출력한다.
 */
 
+#include <stdio.h>
+
+int main(void)
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", a*b);
+    
+    return 0;
+}
