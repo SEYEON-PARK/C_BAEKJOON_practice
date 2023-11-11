f1a9dc3bcd00207861e36e94fbe2ffe07ba8d3a8
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2023-11-12 01:03:53 +0900

    C언어 BAEKJOON 10103번 문제 풀기!(2)

--- C_10103_2/C_10103_2.c
+++ C_10103_2/C_10103_2.c
@@ -1,2 +1,26 @@
 /*
 */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int A=100, B=100;
+    int roundCount, a, b;
+    
+    scanf("%d", &roundCount);
+    
+    for(int i=0;i<roundCount;i++)
+    {
+        scanf("%d %d", &a, &b);
+        if(a>b)
+            B-=a;
+        else if(b>a)
+            A-=b;
+    }
+    
+    printf("%d\n", A);
+    printf("%d", B);
+    
+    return 0;
+}
