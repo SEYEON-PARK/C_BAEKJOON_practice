e0b097bad054dac5db65ab718db30ba55ae43f54
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2023-12-15 21:51:34 +0900

    C언어 BAEKJOON 2902번 문제 풀기!(1)

--- C_2902_1/C_2902_1.c
+++ C_2902_1/C_2902_1.c
@@ -32,16 +32,16 @@ int main(void)
             result[j]=message[i]; // result[j]에 message[i]의 값 대입하기
             j++; // j에 1 더하기
         }
-        if(message[i]=='-')
+        if(message[i]=='-') // 만약, message[i]가 '-'과 같다면
         {
-            result[j]=message[i+1];
-            j++;
+            result[j]=message[i+1]; // result[j]에 message[i+1]의 값 대입하기
+            j++; // j에 1 더하기
         }
     }
     
-    for(int i=0;i<strlen(result);i++)
+    for(int i=0;i<strlen(result);i++) // 0부터 result 안에 있는 문자열의 길이(NULL 문자 전까지)보다 작을 때까지 1씩 증가시킨 값을 i에 대입하며 반복
     {
-        printf("%c", result[i]);
+        printf("%c", result[i]); // 결과 출력하기
     }
     
     return 0;
