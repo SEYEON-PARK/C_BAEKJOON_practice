4b52bac34048011c51edd5d1a06add77e0837013
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2024-03-13 01:24:33 +0900

    C언어 BAEKJOON 24264번 문제 풀기!(1)

--- C_24264_1/C_24264_1.c
+++ C_24264_1/C_24264_1.c
@@ -21,7 +21,7 @@ MenOfPassion 알고리즘은 다음과 같다.
 int main(void)
 {
     int N; // 변수 선언
-    scanf("%d", &N);
+    scanf("%d", &N); // 사용자로부터 정수 입력받기
     
     printf("%lld\n", (long long)N*N);
     printf("%d", 2);
