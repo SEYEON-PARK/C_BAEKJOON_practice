65ef1b1678b7c203c0dae1922e4ee1e508fd05bb
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2024-03-14 12:15:10 +0900

    C언어 BAEKJOON 2566번 문제 풀기!(1)

--- C_2566_1/C_2566_1.c
+++ C_2566_1/C_2566_1.c
@@ -17,3 +17,29 @@
 
 첫째 줄에 최댓값을 출력하고, 둘째 줄에 최댓값이 위치한 행 번호와 열 번호를 빈칸을 사이에 두고 차례로 출력한다. 최댓값이 두 개 이상인 경우 그 중 한 곳의 위치를 출력한다.
 */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int number[9][9];
+    int big=-1, x, y;
+    for(int i=0;i<9;i++)
+    {
+        for(int j=0;j<9;j++)
+        {
+            scanf("%d", &number[i][j]);
+            if(number[i][j]>big)
+            {
+                big=number[i][j];
+                x=i+1;
+                y=j+1;
+            }
+        }
+    }
+    
+    printf("%d\n", big);
+    printf("%d %d", x, y);
+    
+    return 0;
+}
