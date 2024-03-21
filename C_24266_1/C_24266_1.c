2c35b5aaf031123c76fbfff60b2f4c9f405abec4
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2024-03-22 01:05:53 +0900

    C언어 BAEKJOON 24266번 문제 풀기!(1)

--- C_24266_1/C_24266_1.c
+++ C_24266_1/C_24266_1.c
@@ -16,3 +16,16 @@ MenOfPassion(A[], n) {
 첫째 줄에 코드1 의 수행 횟수를 출력한다.
 둘째 줄에 코드1의 수행 횟수를 다항식으로 나타내었을 때, 최고차항의 차수를 출력한다. 단, 다항식으로 나타낼 수 없거나 최고차항의 차수가 3보다 크면 4를 출력한다.
 */
+
+#include <stdio.h>
+
+int main(void)
+{
+    long long N;
+    scanf("%lld", &N);
+    
+    printf("%lld\n", N*N*N);
+    printf("%d", 3);
+    
+    return 0;
+}
