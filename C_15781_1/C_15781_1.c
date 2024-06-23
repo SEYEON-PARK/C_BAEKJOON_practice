aa11fc93c39889fc6f0d9eec9349298093412fdb
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2024-06-24 01:53:24 +0900

    C언어 BAEKJOON 15781번 문제 풀기!(1)

--- C_15781_1/C_15781_1.c
+++ C_15781_1/C_15781_1.c
@@ -15,8 +15,8 @@
 
 int main(void)
 {
-    int N, M;
-    scanf("%d %d", &N, &M);
+    int N, M; // 변수 선언
+    scanf("%d %d", &N, &M); // 사용자로부터 두 개의 정수 입력받기
     
     int *helmet = (int *)malloc(sizeof(int) * N);
     int *vest = (int *)malloc(sizeof(int) * M);
