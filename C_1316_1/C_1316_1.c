596b13c51dac708115ceec2a433d722902e52443
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2024-07-18 15:30:40 +0900

    C언어 BAEKJOON 1316번 문제 풀기!(1)

--- C_1316_1/C_1316_1.c
+++ C_1316_1/C_1316_1.c
@@ -13,41 +13,41 @@
 
 int main(void)
 {
-    int N;
-    scanf("%d", &N);
-    int count = 0;
+    int N; // 변수 선언
+    scanf("%d", &N); // 사용자로부터 정수 입력받기
+    int count = 0; // 그룹 단어의 개수를 저장할 변수 선언 및 초기화
     
-    for(int i=0;i<N;i++)
+    for(int i=0;i<N;i++) // i에 0부터 N이 되기 전까지 1씩 증가시킨 값을 대입하며 반복
     {
-        int alpha[26]={0};
+        int alpha[26]={0}; // alpha 배열 선언 및 0으로 초기화
         
-        char s[101];
-        scanf("%s", s);
+        char s[101]; // 단어를 저장할 배열 선언
+        scanf("%s", s); // 사용자로부터 문자열 입력받기
         
-        int state = 1;
+        int state = 1; // 상태를 확인할 변수 선언 및 초기화
         
-        for(int j=0;s[j]!=NULL;j++)
+        for(int j=0;s[j]!=NULL;j++) // j가 0부터 s[j]가 NULL이 아니라면 j를 1씩 증가시키며 계속 반복
         {
-            if(alpha[(int)s[j]-97]!=0)
+            if(alpha[(int)s[j]-97]!=0) // 만약, alpha[(int)s[j]-97]가 0이 아니라면(이미 나왔던 적이 있다는 의미)
             {
-                if(s[j]==s[j-1])
-                    continue;
-                else
+                if(s[j]==s[j-1]) // 만약, s[j]가 s[j-1]과 같다면(연속해서 나왔던 거라면)
+                    continue; // 계속 반복
+                else // s[j]가 s[j-1]과 같지 않다면(연속해서 나오지 않았다는 의미)
                 {
-                    state = 0;
-                    break;
+                    state = 0; // state를 0으로 바꾸기
+                    break; // 가장 가까운 반복문 빠져 나가기
                 }
             }
-            else
+            else // alpha[(int)s[j]-97]가 0이라면(이번이 처음으로 나왔다는 의미)
             {
-                alpha[(int)s[j]-97]++;
+                alpha[(int)s[j]-97]++; // alpha[(int)s[j]-97]의 값을 1 증가시키기
             }
         }
         
-        if(state)
-            count++;
+        if(state) // 만약, state가 1로 유지되었다면
+            count++; // count에 1 더하기
     }
     
-    printf("%d", count);
+    printf("%d", count); // 결과 출력하기
     return 0;
 }
