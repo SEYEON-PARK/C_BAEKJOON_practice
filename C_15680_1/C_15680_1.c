c5688db2974970b49687c1117316cd7f3718c247
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2024-07-21 14:27:49 +0900

    C언어 BAEKJOON 15680번 문제 풀기!(1)

--- C_15680_1/C_15680_1.c
+++ C_15680_1/C_15680_1.c
@@ -1,2 +1,25 @@
 /*
+연세대학교의 영문명은 YONSEI, 슬로건은 Leading the Way to the Future이다.
+이를 출력하는 프로그램을 작성해보도록 하자.
+
+첫째 줄에 N이 주어진다. (N = 0 또는 1)
+
+N = 0일 경우: 연세대학교의 영문명을 출력한다.
+N = 1일 경우: 연세대학교의 슬로건을 출력한다.
+대소문자 구별에 주의하도록 하자.
 */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int N;
+    scanf("%d", &N);
+    
+    if(N)
+        printf("Leading the Way to the Future");
+    else
+        printf("YONSEI");
+    
+    return 0;
+}
